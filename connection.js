fdf4e4004e3c05f1ec30f2657f7a0b9c046a02b2
Jenilee_Bahan <bahanjennie@gmail.com>
2024-12-07 09:08:16 +0800

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://bahanjennie:HujBtmme4RPnUYqV@cluster1.gjcx2.mongodb.net/udm_admission',{
-    
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-.then(()=>{
-    console.log('mongoose connected');
-})
-.catch((e)=>{
-    console.log('failed');
-    console.log(e);
-})
+const uri = "mongodb+srv://bahanjennie:<db_password>@cluster1.gjcx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 
 module.exports = mongoose.connection;
