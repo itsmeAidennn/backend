7792362bc7f95beca045645a594ff570a8cffafd
Jenilee_Bahan <bahanjennie@gmail.com>
2024-12-07 08:28:28 +0800

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/udm_admission',{
-    // useNewUrlParser:true,
-    // useUnifiedTopology:true
+mongoose.connect('mongodb+srv://bahanjennie:HujBtmme4RPnUYqV@cluster1.gjcx2.mongodb.net/udm_admission',{
+    
+    useNewUrlParser:true,
+    useUnifiedTopology:true
 })
 .then(()=>{
     console.log('mongoose connected');
